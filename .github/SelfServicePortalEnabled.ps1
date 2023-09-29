62f5b2dc3130a0062c97a44831f2a0cb355c7443
Rin Bytes <53623286+RinBytes@users.noreply.github.com>
2023-09-29 08:11:54 -0400

    Update SelfServicePortalEnabled.ps1

--- .github/SelfServicePortalEnabled.ps1
+++ .github/SelfServicePortalEnabled.ps1
@@ -1,1 +1,8 @@
-
+$VBORPS = Get-VBORestorePortalSettings
+if ($VBORPS.IsServiceEnabled -eq $true) {
+Write-Host "Self-Service Portal is enabled" -ForegroundColor Green
+}
+else
+{
+Write-Host "Self-Service Portal is disabled" -ForegroundColor Yellow
+}
