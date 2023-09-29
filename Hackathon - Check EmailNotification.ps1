700370dedfc17f9a72cf6fd00b01cd41e06efb69
Justin <40319518+faruge@users.noreply.github.com>
2023-09-29 14:03:14 +1000

    Delete Hackathon - Check EmailNotification.ps1

--- Hackathon - Check EmailNotification.ps1
+++
@@ -1,7 +0,0 @@
-$emailSettings = Get-VBOEmailSettings
-if ($emailSettings.EnableNotification -eq $True) {
-	write-host ("Email Notifications is configured.  Please ensure specific settings are configured and tested") -ForegroundColour white
-	} else {
-	write-host "Email Notifications is not configured" -ForegroundColour red
-}
-	
